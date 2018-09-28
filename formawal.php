8c55b9dedb2efa5012c18020ba932f6c3400f92d
ERNISA YUNIARTI SATRIYO PUTRI <43512652+ernisayuniarti@users.noreply.github.com>
2018-09-28 22:02:37 +0700

    Update formawal.php
    FORM AWAL
    ernisa yuniarti satriyo putri
    6701174038
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,25 @@
-<?php
+<h1>FORM LOGIN</h1>
+  <form action="#" method="post">  
+  Username = <input type="text" name="username"><br/><br/>
+  Password = <input type="password" name="password"><br/><br/>
+  <br/>
+  <input type="submit" name="login" value="login">
+  </form>
 
+<?php 
+if (isset($_POST['login'])) {
+  $username=$_POST['username'];
+  $password=$_POST['password'];
 
+  if ($username=="admin"&&$password=="admin") {
+    echo "LOGIN BERHASIL";
+?>
 
+  <a href="proses.php"> LINK KE PROSES </a>
+
+<?php 
+  }else{
+    echo "LOGIN GAGAL";
+  }
+}
 ?>
